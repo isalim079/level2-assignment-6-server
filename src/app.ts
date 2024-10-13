b6b884ddf255dffbf5cc898b658151a5f58e1d3f
isalim079 <salimreza1025@gmail.com>
2024-10-13 21:14:06 +0600

    cors url added

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://recipe-sharing-community-theta.vercel.app"],
     credentials: true,
 }));
 
