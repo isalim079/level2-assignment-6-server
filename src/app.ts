94e74fb093ab2fea3363186beaee7236bf3edd13
isalim079 <salimreza1025@gmail.com>
2024-10-13 20:36:47 +0600

    recipe service and module created

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+    credentials: true,
+}));
 
 // application routes
 app.use('/', UserRoutes)
